1b14d370b612610f3e04ec1ab6979ec87cc96547
Rahul Chandna <rahul.chandna@investec.co.za>
2019-10-23 17:59:26 +0200

    hill climbing and stochastic search

---
+++ src/main/java/localsearch/HillClimbing.java
@@ -0,0 +1,26 @@
+package localsearch;
+
+public class HillClimbing {
+    private static final double START_X = -2;
+
+    public static void main(String[] args) {
+        new HillClimbing().climb();
+    }
+
+    public void climb() {
+        double startingPoint = START_X;
+        double max = f(startingPoint);
+        double index = START_X;
+        double dx = 0.01;
+        while (f(startingPoint + dx) >= max) {
+            System.out.println("Currently at point = " + startingPoint + " function value " + f(startingPoint));
+            startingPoint = startingPoint + dx;
+            max = f(startingPoint);
+        }
+        System.out.println("The maximum value is f(x) = " + max);
+    }
+
+    public double f(double x) {
+        return -(x - 1) * (x - 1) + 2;
+    }
+}
---
+++ src/main/java/localsearch/StochasticSearch.java
@@ -0,0 +1,30 @@
+package localsearch;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class StochasticSearch {
+    private static final double START_X = -2;
+    private static final double END_X = 2;
+
+    public static void main(String[] args) {
+        new StochasticSearch().search();
+    }
+
+    public void search() {
+        double startingPoint = START_X;
+        double min = f(startingPoint);
+        double minIndex = START_X;
+        for (int i = 0; i < 10000000; i++) {
+            final double random = ThreadLocalRandom.current().nextDouble(START_X, END_X);
+            if (f(random) < min) {
+                min = f(random);
+                minIndex = random;
+            }
+        }
+        System.out.println("The minimum value is f(x) = " + min + " at index " + minIndex);
+    }
+
+    public double f(double x) {
+        return (x - 1) * (x - 1) - 1;
+    }
+}

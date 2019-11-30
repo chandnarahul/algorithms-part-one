8ba8ca34dc0827d8ed589b3bd21f82f7b7857f5d
rahul chandna <chandnarahul@yahoo.com>
2019-11-30 19:41:22 +0530

    reduced step size and added logic to calculate best guess

--- src/main/java/simulatedannealing/Constants.java
+++ src/main/java/simulatedannealing/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final double MIN_COORDINATE = -2;
     public static final double MAX_COORDINATE = +2;
     public static final double MIN_TEMPERATURE = 1;
-    public static final double MAX_TEMPERATURE = 100;
+    public static final double MAX_TEMPERATURE = 10000;
+    public static final double COOLING_RATE = 0.002;
 }
--- src/main/java/simulatedannealing/SimulatedAnnealing.java
+++ src/main/java/simulatedannealing/SimulatedAnnealing.java
@@ -12,6 +12,10 @@ public class SimulatedAnnealing {
         this.randomGenerator = new Random();
     }
 
+    public static void main(String[] args) {
+        new SimulatedAnnealing().findOptimum();
+    }
+
     public double f(double x) {
         return (x - 0.3) * (x - 0.3) * (x - 0.3) - 5 * x + x * x - 2;
     }
@@ -32,7 +36,17 @@ public class SimulatedAnnealing {
         double temperature = Constants.MAX_TEMPERATURE;
         while (temperature > Constants.MIN_TEMPERATURE) {
             nextCoordinateX = getRandomX();
+            double actualEnergy = functionEnergy(currentCoordinateX);
+            double newEnergy = functionEnergy(nextCoordinateX);
+            if (acceptanceProbability(actualEnergy, newEnergy, temperature) > Math.random()) {
+                currentCoordinateX = nextCoordinateX;
+            }
+            if (f(currentCoordinateX) < f(bestCoordinateX)) {
+                bestCoordinateX = currentCoordinateX;
+            }
+            temperature *= 1 - Constants.COOLING_RATE;
         }
+        System.out.println("Global extremum guess: x=" + bestCoordinateX + " f(x)=" + f(bestCoordinateX));
     }
 
     private double getRandomX() {
